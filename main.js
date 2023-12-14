7aa524280caf2a483cd5143565b8c9f168b26df3
Vladimir Semenyako <2243880@gmail.com>
2023-12-15 00:45:57 +0300

    ‘update’

---
+++ main.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+navBtn.onclick = () => {
+  if (nav.classList.toggle('open')) {
+    navBtnImg.src = "./img/mobile-menu/menu-close.svg"
+  } else {
+navBtnImg.src = "./img/mobile-menu/menu-open.svg"
+  }
+}
