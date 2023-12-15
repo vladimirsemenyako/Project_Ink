f1cedee841130d2b688c8c04db6a1c1c7e0cf4ac
Vladimir Semenyako <2243880@gmail.com>
2023-12-15 10:30:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
+const headerNav = document.querySelector('#header-nav');
 navBtn.onclick = () => {
   if (nav.classList.toggle('open')) {
-    navBtnImg.src = "./img/mobile-menu/menu-close.svg"
+    navBtnImg.src = "./img/mobile-menu/menu-close.svg";
+    headerNav.style.justifyContent = "center";
   } else {
 navBtnImg.src = "./img/mobile-menu/menu-open.svg"
+headerNav.style.justifyContent = "space-between";
   }
 }
